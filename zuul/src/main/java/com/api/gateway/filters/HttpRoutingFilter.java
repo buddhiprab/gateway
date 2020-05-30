a3657dff4c3eaa73c460910d5e3e647d6e4b0b33
buddhi <buddhi.1986@gmail.com>
2020-05-31 00:47:02 +0800

    init

---
+++ zuul/src/main/java/com/api/gateway/filters/HttpRoutingFilter.java
@@ -0,0 +1,32 @@
+package com.api.gateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.ROUTE_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SIMPLE_HOST_ROUTING_FILTER_ORDER;
+
+public class HttpRoutingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return ROUTE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SIMPLE_HOST_ROUTING_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return RequestContext.getCurrentContext().getRouteHost() != null
+                && RequestContext.getCurrentContext().sendZuulResponse();
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("filter hit");
+        return null;
+    }
+}
