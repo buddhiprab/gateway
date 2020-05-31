3a2ee99e4401609e60be2f94bb66490b8a9d3b07
buddhi <buddhi.1986@gmail.com>
2020-05-31 15:21:12 +0800

    service2, service3

--- service1/src/main/java/com/api/service1/controllers/ServiceController.java
+++ service1/src/main/java/com/api/service1/controllers/ServiceController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*created by Buddhi*/
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceController {
 
-    @GetMapping("/test")
-    public ResponseEntity test() {
-        return new ResponseEntity("/service1/test", HttpStatus.OK);
+    @GetMapping("/test1")
+    public ResponseEntity test(@RequestParam(name = "param1") String param1) {
+        return new ResponseEntity("/service1/test1?param1="+param1, HttpStatus.OK);
     }
 
 }
